e875f16e65de15357786db813fb83ebfd40c6393
Pierce Jorgensen <jorgensenpierce@gmail.com>
2026-09-11 19:26:30 -0400

    TRUST-9 removed JS training from this repo

--- moviedux/src/App.js
+++ moviedux/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       
       <div className='container'>
         <Header></Header>
+        <MoviesGrid></MoviesGrid>
       </div>
 
       <div className='container'>
