2e402fe4067cdc2147876c322fca90fca74b9ce1
Pierce Jorgensen <jorgensenpierce@gmail.com>
2026-09-13 00:00:37 -0400

    TRUST-11 completed section 6

--- moviedux/src/App.js
+++ moviedux/src/App.js
@@ -5,8 +5,24 @@ import Footer from './components/Footer';
 import MoviesGrid from './components/MoviesGrid';
 import Watchlist from "./components/Watchlist";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  useEffect(() => {
+      fetch("movies.json")
+      .then(response => response.json())
+      .then(data => setMovies(data))
+  }, []);
+
+  const toggleWatchlist = (movieId) => {
+    setWatchlist(prev => 
+      prev.includes(movieId) ? prev.filter(id => id !== movieId) : [...prev, movieId]
+    )
+  }
+
   return (
     <div className="App">
       <div className='container'>
@@ -19,14 +35,20 @@ function App() {
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/watchlist"></Link>
+                <Link to="/watchlist">Watchlist</Link>
               </li>
             </ul>
           </nav>
 
           <Routes>
-            <Route path="/" element={<MoviesGrid />}></Route>
-            <Route path="/watchlist" element={<Watchlist />}></Route>
+            <Route 
+              path="/" 
+              element={<MoviesGrid movies={movies} watchlist={watchlist} toggleWatchlist={toggleWatchlist}/>}>
+            </Route>
+            <Route 
+              path="/watchlist" 
+              element={<Watchlist movies={movies} watchlist={watchlist} toggleWatchlist={toggleWatchlist}/>}>
+            </Route>
           </Routes>
         </Router>
       </div>
