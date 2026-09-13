123296815f60a2bf76119b1dedbb56d860e9d3ca
Pierce Jorgensen <jorgensenpierce@gmail.com>
2026-09-13 17:01:29 -0400

    TRUST-12 created react app for section 7

---
+++ bugblaster/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import "./styles.css";
+import TicketForm from './components/TicketForm';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bug Blaster</h1>
+        <TicketForm></TicketForm>
+      </div>
+    </div>
+  );
+}
+
+export default App;
