f5a64187be44d38f2a60a4a4c48ca8b5513e9016
Pierce Jorgensen <jorgensenpierce@gmail.com>
2026-09-18 18:57:34 -0400

    TRUST-29 created vite react project for section 14

---
+++ tech-bros/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Clerk publishable key missing!");
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider>
+      <App />
+    </ClerkProvider>
+  </StrictMode>,
+)
