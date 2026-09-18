5bcd91ecde799958379e05e1b404fe8ec6d85f29
Pierce Jorgensen <jorgensenpierce@gmail.com>
2026-09-18 15:40:21 -0400

    TRUST-13 completed section 11

--- react-api-demo/src/components/PostForm.js
+++ react-api-demo/src/components/PostForm.js
@@ -1,23 +1,56 @@
-import React, { useState } from "react";
-import { createPost } from "../services/postService";
+import React, { useState, useEffect } from "react";
+import { createPost, updatePost } from "../services/postService";
 
-export default function PostForm({ posts, setPosts }) {
+export default function PostForm({
+    posts,
+    setPosts,
+    editingPost,
+    setEditingPost
+}) {
 
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
 
+    useEffect(() => {
+
+        if (editingPost) {
+            setTitle(editingPost.title);
+            setBody(editingPost.body)
+        } else {
+            setTitle("");
+            setBody("");
+        }
+
+    }, [editingPost])
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        addPost();
+        if (editingPost) {
+            editPost();
+        } else {
+            addPost();
+        }
+
         setTitle("");
         setBody("");
+        setEditingPost(null);
     }
 
     const addPost = () => {
         createPost({ title, body })
             .then((response) => {
                 setPosts([...posts, response.data])
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }
+
+    const editPost = () => {
+        updatePost(editingPost.id, { title, body })
+            .then((response) => {
+                setPosts(posts.map(post => (post.id === editingPost.id ? response.data : post)));
             })
             .catch((err) => {
                 console.error(err)
@@ -42,7 +75,7 @@ export default function PostForm({ posts, setPosts }) {
                 onChange={(e) => setBody(e.target.value)}
             ></textarea>
             <div>
-                <button type="submit">Add Post</button>
+                <button type="submit">{editingPost ? "Edit Post" : "Add Post"}</button>
             </div>
         </form>
     );
