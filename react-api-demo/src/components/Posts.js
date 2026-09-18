e5e84de6f79b99d2846e59018da6c841584fc678
Pierce Jorgensen <jorgensenpierce@gmail.com>
2026-09-17 23:19:36 -0400

    TRUST-13 added mock API using axios

---
+++ react-api-demo/src/components/Posts.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { getPosts } from '../services/postService';
+
+export default function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        getPosts()
+            .then(result => {
+                setPosts(result.data)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }, []);
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {
+                    posts.map(post => (
+                        <li key={post.id}>
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+}
