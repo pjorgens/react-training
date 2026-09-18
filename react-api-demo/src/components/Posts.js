d02266094e180cca8b2fd585273e3a01316bdd60
Pierce Jorgensen <jorgensenpierce@gmail.com>
2026-09-18 14:06:23 -0400

    TRUST-13 added create methods

--- react-api-demo/src/components/Posts.js
+++ react-api-demo/src/components/Posts.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getPosts, deletePost } from '../services/postService';
+import PostForm from './PostForm';
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
@@ -34,6 +35,7 @@ export default function Posts() {
     return (
         <div>
             <h1>Posts</h1>
+            <PostForm posts={posts} setPosts={setPosts}></PostForm>
             <ul>
                 {
                     posts.map(post => (
